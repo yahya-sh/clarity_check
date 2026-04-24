dc55e5bcea143bb384f8d85d701005b91e347de4
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-04-25 01:16:41 +0300

    Setup CSRF protection using Flask-WTF

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
+from flask_wtf.csrf import CSRFProtect
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
+csrf = CSRFProtect(app)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'insecure-bc054a63d0f9c2b537d4b0f6bebadb3630dd73495a140241')
+
 @app.route('/')
 def index():
     return render_template('index.html')
