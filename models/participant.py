038b1d7f2b0dd92456358d0f41b997ad07503725
Yahya Alshalabi <yahya.alshalabi@hotmail.com>
2026-04-28 19:35:33 +0300

    Update Participant model to include presentation context

--- models/participant.py
+++ models/participant.py
@@ -5,17 +5,23 @@ class Participant:
     uuid: str
     session_uuid: str
     nickname: str
+    presentation_uuid: str
+    presentation_instructor_username: str
     
-    def __init__(self, session_uuid: str, nickname: str, participant_uuid: str|None = None):
+    def __init__(self, session_uuid: str, nickname: str, presentation_uuid: str, presentation_instructor_username: str, participant_uuid: str|None = None):
         self.uuid = participant_uuid or str(uuid.uuid4())
         self.session_uuid = session_uuid
         self.nickname = nickname
+        self.presentation_uuid = presentation_uuid
+        self.presentation_instructor_username = presentation_instructor_username
     
     def to_dict(self):
         return {
             'uuid': self.uuid,
             'session_uuid': self.session_uuid,
             'nickname': self.nickname,
+            'presentation_uuid': self.presentation_uuid,
+            'presentation_instructor_username': self.presentation_instructor_username,
         }
     
     @classmethod
@@ -23,5 +29,7 @@ class Participant:
         return cls(
             session_uuid=data['session_uuid'],
             nickname=data['nickname'],
+            presentation_uuid=data['presentation_uuid'],
+            presentation_instructor_username=data['presentation_instructor_username'],
             participant_uuid=data['uuid']
         )
